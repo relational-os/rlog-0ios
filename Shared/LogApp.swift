46e81f3c70f0b49ce5e5dad7a72aa382dc1cc85d
CJ Pais <cj@cjpais.com>
2022-07-08 14:24:30 -0700

    Initial Commit

---
+++ Shared/LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogApp.swift
+//  Shared
+//
+//  Created by CJ Pais on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
